803cee6bc120a4164971e670f68f0368fd904f84
Edward Nguyen <ed@delimister.com>
2023-12-24 07:33:36 +0700

    sider icons

--- src/components/Sider.tsx
+++ src/components/Sider.tsx
@@ -1,7 +1,20 @@
 import styles from "./Sider.module.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCalendar, faEnvelope, faHeart, faPencil} from "@fortawesome/free-solid-svg-icons";
 
 export default function Sider() {
   return <div className={styles.sider}>
-
+    <a className={styles.item}>
+      <FontAwesomeIcon icon={faEnvelope}/>
+    </a>
+    <a className={styles.item}>
+      <FontAwesomeIcon icon={faCalendar}/>
+    </a>
+    <a className={styles.item}>
+      <FontAwesomeIcon icon={faHeart}/>
+    </a>
+    <a className={styles.item}>
+      <FontAwesomeIcon icon={faPencil}/>
+    </a>
   </div>
 }
